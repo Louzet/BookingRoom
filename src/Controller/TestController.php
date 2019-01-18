fa629e78b6210cb78bd4ddcf897622b2de9b497c
Louzet <micklouzet@hotmail.fr>
2019-01-18 17:33:19 +0100

    add branch, et option LDAP

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-
 use Adldap\Adldap;
 use Adldap\Auth\BindException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,7 +9,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class TestController extends AbstractController
 {
-
     /**
      * @Route("/ldap-test", name="ldap.test")
      */
@@ -20,15 +18,15 @@ class TestController extends AbstractController
         $ad = new Adldap();
 
         $config = [
-            'hosts'    => ['ldap.agopreneur.fr'],
-            'base_dn'  => 'dc=ldap,dc=agopreneur,dc=fr',
+            'hosts' => ['ldap.agopreneur.fr'],
+            'base_dn' => 'dc=ldap,dc=agopreneur,dc=fr',
             'username' => 'cn=admin,dc=ldap,dc=agopreneur,dc=fr',
             'password' => 'nimdaLDAP_1',
-            'port'             => 389,
+            'port' => 389,
             'follow_referrals' => false,
-            'use_ssl'          => true,
-            'use_tls'          => false,
-            'version'          => 3,
+            'use_ssl' => true,
+            'use_tls' => false,
+            'version' => 3,
         ];
 
         // Add a connection provider to Adldap.
@@ -40,28 +38,23 @@ class TestController extends AbstractController
             dump($provider);
 
             // Creating a new LDAP entry. You can pass in attributes into the make methods.
-            $user =  $provider->make()->user();
+            $user = $provider->make()->user();
 
             $user->setCommonName('John Doe3');
-            $user->setPassword("test");
-            $user->setLastName("Doe3");
-            $user->setFirstName("John");
+            $user->setPassword('test');
+            $user->setLastName('Doe3');
+            $user->setFirstName('John');
             $user->setHomeDirectory('/home/users/jo');
-            $user->setEmail("jojo@gmail.com");
+            $user->setEmail('jojo@gmail.com');
 
             // Saving the changes to your LDAP server.
             if ($user->save()) {
-
-                $this->addFlash("success", "user created");
-
+                $this->addFlash('success', 'user created');
             }
-
         } catch (BindException $e) {
-
             dump($e);
-
         }
 
-        return $this->render("test/ldap-test.html.twig");
+        return $this->render('test/ldap-test.html.twig');
     }
 }
